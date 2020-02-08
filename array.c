9b77182bf010fff7b97a88506ce8adf15c5f3765
dronai8989 <60754385+dronai8989@users.noreply.github.com>
2020-02-09 02:15:03 +0600

    Add files via upload

---
+++ array.c
@@ -0,0 +1,20 @@
+//c program to read and print Negative elements in an array
+#include<stdio.h>
+int main()
+{
+    int arr[5];
+    int i,n;
+    printf("Enter size of array : ");
+    scanf("%d",&n);
+    printf("Enter %d elements in the array : ",n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Negative elements in array : ");
+    for(i=0;i<n;i++)
+    {
+        printf("%d,",arr[i]);
+    }
+    return 0;
+}
